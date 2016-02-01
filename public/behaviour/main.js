fb63551528f6a18ada49e656a07ffccd12f7a623
Seb Insua <me@sebinsua.com>
2016-02-01 15:52:31 +0000

    wip(initial): pulled 2014 www into new project

--- public/behaviour/main.js
+++ public/behaviour/main.js
@@ -1,25 +1,56 @@
+
+
 $(document).ready(function () {
+    var transform = function ($clouds, $headline, $box) {
+        $clouds.addClass('darken');
+        $headline.fadeOut(500, function () {
+            $box.fadeIn({ duration: 1000, easing: 'swing'});
+        });
+    };
+    var dance = function () {
+        var $clouds = $('.clouds'),
+            $box = $('.box'),
+            $headline = $('.headline');
+
+        if (!$box.is(':visible')) {
+            setTimeout(function () {
+                transform($clouds, $headline, $box);
+            }, 1500);
+        }
+    };
+
+    var $video = $('#v-background'),
+        $space = $('#space'),
+        $logo = $('#logo');
+
     var isMobile = navigator.userAgent.match(/Android|iPhone|iPod/i) != null;
     if (isMobile) {
-        $('#v-background').hide();
+        $video.hide();
     }
 
-    $('#space').parallaxify({
-        responsive: true,
-        useMouseMove: false
+    $space.parallaxify({
+        responsive: false,
+        useMouseMove: true,
+        positionProperty: 'transform',
+        alphaFilter: 0.8,
+        motionType: 'natural'
     });
 
-    if (!isMobile) {
-        var background = $('#v-background');
-        var logo = $('#logo');
-        var targetHeight = background.height();
-        var minTargetHeight = targetHeight * 0.7;
-        var standardBackgroundOpacity = 0.6;
-        var standardLogoOpacity = 1.0;
+    var background = $video;
+    var logo = $logo;
 
-        $(document).scroll(function (e) {
-            var scrollY = $(window).scrollTop();
-            var scrollPercent = scrollY / minTargetHeight;
+    var targetHeight = background.height();
+    var minTargetHeight = targetHeight * 1.0;
+    var standardBackgroundOpacity = 0.6;
+    var standardLogoOpacity = 1.0;
+
+    $(document).scroll(function (e) {
+
+        var scrollY = $(window).scrollTop();
+        var scrollPercent = scrollY / minTargetHeight;
+
+        if (!isMobile) {
+
             var backgroundOpacity = standardBackgroundOpacity - scrollPercent;
             var logoOpacity = standardLogoOpacity - scrollPercent;
             if (backgroundOpacity < 0) {
@@ -31,6 +62,10 @@ $(document).ready(function () {
 
             background.css('opacity', backgroundOpacity);
             logo.css('opacity', logoOpacity);
-        });
-    }
+        }
+
+        if (scrollPercent >= 0.75) {
+            dance();
+        }
+    });
 });
--- web.js
+++ web.js
@@ -1,16 +1,31 @@
-var express = require("express");
+var express = require("express"),
+    keenio = require('express-keenio');
+
 var app = express();
+
+keenio.configure({
+    client: {
+        projectId: '52e5d99dce5e431e34000007',
+        writeKey: 'abbbca1201fd62369c6fbff9f042420f87ca2167a90feed220f832c37aff09db1c05247e4b50eb976421fe5888279ad04ba1c96a0b2b144d0afbda81b8829152d38958442ead2ae02355b2a3f180da998be890eb68931f8f5e6f1438943aa7add68b60ea68b39e8a6e23fc10054d79fd'
+    },
+    whitelistProperties: {
+        query: [],
+        body: [],
+        reaction: []
+    }
+});
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.logger());
+app.use(keenio);
 app.set('view engine', 'jade');
 app.set('views', './views');
 
 app.get('/', function(request, response) {
-    response.render('home.jade');
+    response.render('home.jade', { currentYear: new Date().getFullYear() });
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-    console.log("Listening on " + port);
+var PORT = process.env.PORT || 5000;
+app.listen(PORT, function() {
+    console.log("Listening on " + PORT);
 });
